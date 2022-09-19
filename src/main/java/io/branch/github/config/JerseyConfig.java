e43d755d27724e2e3692a8389033e2b4f70e37de
Whitney Heastie <whitney.heastie@resi.io>
2022-09-18 21:43:53 -0500

    Remove PingResource

--- src/main/java/io/branch/github/config/JerseyConfig.java
+++ src/main/java/io/branch/github/config/JerseyConfig.java
@@ -4,7 +4,6 @@ import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvi
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
-import io.branch.github.PingResource;
 import io.branch.github.commons.exception.mapper.FallbackExceptionMapper;
 import io.branch.github.commons.exception.mapper.GithubAccessForbiddenExceptionMapper;
 import io.branch.github.commons.exception.mapper.GithubUserNotFoundExceptionMapper;
@@ -15,7 +14,6 @@ public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
 		register(JacksonJsonProvider.class);
 		register(ProfileResource.class);
-		register(PingResource.class);
 		register(FallbackExceptionMapper.class);
 		register(GithubAccessForbiddenExceptionMapper.class);
 		register(GithubUserNotFoundExceptionMapper.class);
